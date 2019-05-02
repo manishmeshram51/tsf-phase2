05f9cb0c89c5a279266922901ae4821e5eef579d
manishmeshram51 <manishmeshram51@gmail.com>
2019-05-02 17:30:27 +0530

    new

--- DRF_tsf/myapp/admin.py
+++ DRF_tsf/myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import employee
+from .models import employee, student, teacher
 
 # Register your models here.
 admin.site.register(employee)  
+admin.site.register(student) 
+admin.site.register(teacher) 
