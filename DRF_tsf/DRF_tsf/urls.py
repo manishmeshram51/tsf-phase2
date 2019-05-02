4edf3962fe1e46ba49d5e945bfa2304b75224551
manishmeshram51 <manishmeshram51@gmail.com>
2019-05-03 02:51:22 +0530

    update

--- DRF_tsf/DRF_tsf/urls.py
+++ DRF_tsf/DRF_tsf/urls.py
@@ -15,6 +15,6 @@ router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls)),#api url
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
